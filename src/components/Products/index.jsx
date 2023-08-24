f1548f3044e7d2e0b208afff6fc4f84367487b3c
natanael silva <natanlimer@gmail.com>
2023-08-24 16:49:52 -0300

    fixing problems with routes

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -1,25 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 import { Hero } from "../Hero";
 import Card from "../Card";
 
 export const Products = () => {
   const { categoriesName } = useParams();
-  const { categorie, filteredItems } = useSelector((state) => ({
-    categorie: state.categories.find(
-      (categoria) => categoria.id === categoriesName
-    ),
+  const navigate = useNavigate();
+  const { categorie, filteredItems } = useSelector((state) => {
+    return {
+      categorie: state.categories.find(
+        (categoria) => categoria.id === categoriesName
+      ),
 
-    filteredItems: state.filteredItems.filter(
-      (item) => item.categorie === categoriesName
-    ),
-  }));
+      filteredItems: state.filteredItems.filter(
+        (item) => item.categorie === categoriesName
+      ),
+    };
+  });
+
+  useEffect(() => {
+    if (categorie === undefined) {
+  
+      navigate("/");
+      return; 
+    }
+  }, [categoriesName]);
 
   return (
     <div>
-      <Hero title={categorie.name} image={categorie.img} />
+      <Hero title={categorie?.name} image={categorie?.img} />
       <section className="flex flex-wrap items-center justify-center gap-11">
         {filteredItems?.map((el) => (
           <Card
